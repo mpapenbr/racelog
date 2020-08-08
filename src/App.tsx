1bc2f2f3f278e8f12750415e92825c29589a8394
mpapenbr <Markus.Papenbrock@gmail.com>
2020-08-08 23:03:05 +0200

    Initial preps

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./App.css";
+import logo from "./logo.svg";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <p>Mode is {process.env.NODE_ENV}</p>
+        <p>Value is {process.env.REACT_APP_MY_VAR}</p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
